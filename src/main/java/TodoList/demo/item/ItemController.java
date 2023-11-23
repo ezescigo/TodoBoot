555348ee569766dc475f43c206df77db38ead8ca
Ezequiel Scigolini <scigolini@gmail.com>
2023-11-24 00:42:31 +0100

    POST + DELETE

--- src/main/java/TodoList/demo/item/ItemController.java
+++ src/main/java/TodoList/demo/item/ItemController.java
@@ -1,9 +1,7 @@
 package TodoList.demo.item;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,13 @@ public class ItemController {
         return itemService.getItems();
     }
 
+    @PostMapping
+    public void createItem(@RequestBody Item item) {
+            itemService.createNewItem(item);
+    }
+
+    @DeleteMapping(path = "{itemId}")
+    public void deleteItem(@PathVariable("itemId") Long itemId) {
+        itemService.deleteItem(itemId);
+    }
 }
--- src/main/java/TodoList/demo/item/ItemService.java
+++ src/main/java/TodoList/demo/item/ItemService.java
@@ -15,4 +15,16 @@ public class ItemService {
     public List<Item> getItems() {
         return itemRepository.findAll();
     }
+
+    public void createNewItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public void deleteItem(Long itemId) {
+        boolean exists = itemRepository.existsById(itemId);
+        if (!exists) {
+            throw new IllegalStateException("item with id " + itemId + " does not exists.");
+        }
+        itemRepository.deleteById(itemId);
+    }
 }
